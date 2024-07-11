870d726ab45df2894e2906ca48808bb6a24af5c8
Jacques Ngome Heffa <ngomeheffa@yahoo.fr>
2024-07-11 09:37:10 +0200

    create package

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,4 @@
+import type { IMapboxNavigationProps } from './types';
+declare const MapboxNavigation: import("react-native").HostComponent<IMapboxNavigationProps> | (() => never);
+export default MapboxNavigation;
+//# sourceMappingURL=index.d.ts.map
